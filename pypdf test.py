2ccb337e91b1d26db59936f301cce75bae091936
Bloomuel <s.bloomuel@gmail.com>
2025-03-24 06:09:48 -0400

    Edits to the previous files pushed eariler -> parsing reformatted, please look at 2nd function and fix the dictionary.

--- pypdf test.py
+++ pypdf test.py
@@ -11,17 +11,51 @@ def extract_text_from_pdf(pdf_path):
         for page_num in range(num_pages):
             page = pdf_reader.pages[page_num]
             page_text = page.extract_text()
-            full_text += page_text  
+            full_text += page_text 
         return full_text
+    
+# Organizing the .txt file, such that the information is sorted into dictionaries
+def organizing_incidents_text(incidents_text):
+    columns = [
+        "S/N", 
+        "Name of ship",
+        "Incident Type",
+        "Date of incident",
+        "Time of incident",
+        "Flag of ship",
+        "Type of ship",
+        "Latitude",
+        "Longitude",
+        "Area location",
+        "Area description",
+        "Attack method & description of incident",
+        "CAT"
+    ]
+    
+    parsed_data = []
+    
+    # reformatting, such that we can place values into a dictionary.
+    split_text = incidents_text.split("  ")  # Split based on double spaces
+    split_text = [part.replace("\n", " ").strip() for part in split_text]
 
-# Example usage
+    for i in range(0, len(split_text), 13):
+        row_data = {}
+        current_row = split_text[i:i + 13]
+        
+        for j, part in enumerate(current_row):
+            row_data[columns[j]] = part 
+        
+        parsed_data.append(row_data)
+        
+    return parsed_data
+
+# Calling the function
 PDF_PATH = "/mnt/c/Users/sbloom/Downloads/Pirate_Incidents_2024.pdf"
 extracted_text = extract_text_from_pdf(PDF_PATH)
+
+refromatted_incidents = organizing_incidents_text(extracted_text)
+
+with open("parsed_pirate_incidents.json", "w", encoding="utf-8") as json_file:
+        json.dump(refromatted_incidents, json_file, indent=4)
     
-# Save the extracted text to a file
-with open("PYPDF_extracted_text.txt", "w", encoding="utf-8") as text_file:
-    text_file.write(extracted_text)
-    print("Text extraction complete!")
-
-# Organizing the .txt file, such that the information is sorted into dictionaries
-
+print("JSON file 'parsed_pirate_incidents.json' has been created.")
