af7e22ddc4088e5984197204815a48ad09adf48c
Hong Yi Zhang <hongyizhang@Mac.olin.edu>
2025-03-25 14:21:04 -0400

    Added Photo for Heat Map

--- iteration_of_pdf_parser/data_to_dataframes.py
+++ iteration_of_pdf_parser/data_to_dataframes.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
-# Load the renamed CSV
 df = pd.read_csv("Lat_Long_Description.csv")
 
 # Get the Index column as a list
 index_list = df["Index"].tolist()
 
-# Print it in the terminal
+# Get the Latitude column as a list
+Latitude_list = df["Latitude"].tolist()
+
+# Get the Longitude column as a list
+Longitude_list = df["Longitude"].tolist()
+
+# Get the Description column as a list
+Description_list = df["Description"].tolist()
+
 print(index_list)
